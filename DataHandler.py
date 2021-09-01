d441e9943d225ec646b05332060aa26fb23d6990
Ved <ved789@gmail.com>
2021-09-01 13:20:13 +0530

    Add files via upload

---
+++ DataHandler.py
@@ -0,0 +1,166 @@
+import glob
+import shutil
+import os
+import random
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+from google.cloud import storage
+
+path_to_credentials = './credentials/train-deep-learning-models-ebca05976b3a.json' # with storage admin role
+
+food_classes = ['broken_street_sign', 'damaged_bollard', 'damaged_street_light', 'pothole']
+
+
+def split_data_into_class_folders(path_to_data, class_id):
+
+    imgs_paths = glob.glob(path_to_data + '*.jpg')
+
+    for path in imgs_paths:
+
+        basename = os.path.basename(path)
+
+        if basename.startswith(str(class_id) + '_'):
+
+            path_to_save = os.path.join(path_to_data, food_classes[class_id])
+
+            if not os.path.isdir(path_to_save):
+                os.makedirs(path_to_save)
+
+            shutil.move(path, path_to_save)
+
+
+def visualize_some_images(path_to_data):
+
+    imgs_paths = []
+    labels = []
+
+    for r, d, f in os.walk(path_to_data):
+        for file in f:
+            if file.endswith(".jpg"):
+                imgs_paths.append(os.path.join(r, file))
+                labels.append(os.path.basename(r))
+
+    fig = plt.figure()
+
+    for i in range(16):
+        chosen_index = random.randint(0, len(imgs_paths)-1)
+        chosen_img = imgs_paths[chosen_index]
+        chosen_label = labels[chosen_index]
+
+        ax = fig.add_subplot(4,4, i+1)
+        ax.title.set_text(chosen_label)
+        ax.imshow(Image.open(chosen_img))
+
+    fig.tight_layout(pad=0.05)
+    plt.show()
+
+
+def get_images_sizes(path_to_data):
+
+    imgs_paths = []
+    widths = []
+    heights = []
+
+    for r, d, f in os.walk(path_to_data):
+        for file in f:
+            if file.endswith(".jpg"):
+
+                img = Image.open(os.path.join(r, file))
+                widths.append(img.size[0])
+                heights.append(img.size[1])
+                img.close()
+
+    mean_width = sum(widths) / len(widths)
+    mean_height = sum(heights) / len(heights)
+    median_width = np.median(widths)
+    median_height = np.median(heights)
+
+    return mean_width, mean_height, median_width, median_height
+
+
+def list_blobs(bucket_name):
+
+    storage_client = storage.Client.from_service_account_json(path_to_credentials)
+
+    blobs = storage_client.list_blobs(bucket_name)
+
+    return blobs
+
+def download_data_to_local_directory(bucket_name, local_directory):
+
+    storage_client = storage.Client.from_service_account_json(path_to_credentials)
+    blobs = storage_client.list_blobs(bucket_name)
+
+    if not os.path.isdir(local_directory):
+        os.makedirs(local_directory)
+
+    for blob in blobs:
+
+        joined_path = os.path.join(local_directory, blob.name)
+
+        if os.path.basename(joined_path) == '':
+            if not os.path.isdir(joined_path):
+                os.makedirs(joined_path)
+
+        else:
+            if not os.path.isfile(joined_path):
+                if not os.path.isdir(os.path.dirname(joined_path)):
+                    os.makedirs(os.path.dirname(joined_path))
+                    
+                blob.download_to_filename(joined_path)
+
+def upload_data_to_bucket(bucket_name, path_to_data, bucket_blob_name):
+
+    storage_client = storage.Client.from_service_account_json(path_to_credentials)
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blob = bucket.blob(bucket_blob_name)
+    blob.upload_from_filename(path_to_data)
+
+
+if __name__ == '__main__':
+
+    split_data_switch = False
+    visualize_data_switch = True
+    print_insights_switch = False
+    list_blobs_switch = False
+    download_data_switch = False
+
+    path_to_train_data = './data/training/'
+    path_to_val_data = './data/training/'
+    path_to_eval_data = './data/training/'
+
+    
+    if split_data_switch :
+        for i in range(4):
+            split_data_into_class_folders(path_to_train_data, i)
+        for i in range(4):
+            split_data_into_class_folders(path_to_val_data, i)
+        for i in range(4):
+            split_data_into_class_folders(path_to_eval_data, i)
+
+    if visualize_data_switch:
+        visualize_some_images(path_to_train_data)
+
+
+    if print_insights_switch:
+        mean_width, mean_height, median_width, median_height = get_images_sizes(path_to_train_data)
+
+        print(f"mean width = {mean_width}")
+        print(f"mean height = {mean_height}")
+        print(f"median width = {median_width}")
+        print(f"median height = {median_height}")
+
+
+    if list_blobs_switch:
+        blobs = list_blobs('images-data-bucket')
+
+        for blob in blobs:
+            print(blob.name)
+
+    if download_data_switch:
+        download_data_to_local_directory("images-data-bucket", "./data")
+
+
---
+++ trainer.py
@@ -0,0 +1,216 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.layers import Dropout, Flatten, Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+import hypertune
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+import os
+import numpy as np
+
+from DataHandler import download_data_to_local_directory, upload_data_to_bucket
+
+from tensorflow.python.client import device_lib
+import argparse
+import shutil
+from datetime import datetime
+
+print("Tensorflow is running on following devices : ")
+print(device_lib.list_local_devices())
+
+
+def build_model(nbr_classes):
+
+    base_model = InceptionV3(weights="imagenet", include_top=False, input_tensor=Input(shape=(229, 229, 3)))
+
+    head_model = base_model.output
+    head_model = Flatten()(head_model)
+    head_model = Dense(512)(head_model)
+    head_model = Dropout(0.5)(head_model)
+    head_model = Dense(nbr_classes, activation="softmax")(head_model)
+
+    model = Model(inputs=base_model.input, outputs=head_model)
+
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    return model
+
+
+
+def build_data_pipelines(batch_size, train_data_path, val_data_path, eval_data_path):
+
+    train_augmentor = ImageDataGenerator(
+        rescale = 1. / 255,
+        rotation_range=25,
+        zoom_range=0.15,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.15,
+        horizontal_flip=True,
+        fill_mode="nearest"
+    )
+
+    val_augmentor = ImageDataGenerator(
+        rescale = 1. / 255
+    )
+
+    train_generator = train_augmentor.flow_from_directory(
+        train_data_path,
+        class_mode="categorical",
+        target_size=(229,229),
+        color_mode="rgb",
+        shuffle=True,
+        batch_size=batch_size
+    )
+
+    val_generator = val_augmentor.flow_from_directory(
+        val_data_path,
+        class_mode="categorical",
+        target_size=(229,229),
+        color_mode="rgb",
+        shuffle=False,
+        batch_size=batch_size
+    )
+
+    eval_generator = val_augmentor.flow_from_directory(
+        eval_data_path,
+        class_mode="categorical",
+        target_size=(229,229),
+        color_mode="rgb",
+        shuffle=False,
+        batch_size=batch_size
+    )
+
+
+    return train_generator, val_generator, eval_generator
+
+
+def get_number_of_imgs_inside_folder(directory):
+
+    totalcount = 0
+
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            _, ext = os.path.splitext(filename)
+            if ext in [".png", ".jpg", ".jpeg"]:
+                totalcount = totalcount + 1
+
+    return totalcount
+
+
+def train(path_to_data, batch_size, epochs, learning_rate, models_bucket_name):
+
+    path_train_data = os.path.join(path_to_data, 'training')
+    path_val_data = os.path.join(path_to_data, 'training') # validation
+    path_eval_data = os.path.join(path_to_data, 'training') # evaluation
+
+    total_train_imgs = get_number_of_imgs_inside_folder(path_train_data)
+    total_val_imgs = get_number_of_imgs_inside_folder(path_val_data)
+    total_eval_imgs = get_number_of_imgs_inside_folder(path_eval_data)
+
+    print(total_train_imgs, total_val_imgs, total_eval_imgs)
+    
+
+    train_generator, val_generator, eval_generator = build_data_pipelines(
+        batch_size=batch_size,
+        train_data_path=path_train_data,
+        val_data_path=path_val_data,
+        eval_data_path=path_eval_data
+    )
+
+    classes_dict = train_generator.class_indices
+
+    model = build_model(nbr_classes=len(classes_dict.keys()))
+
+    optimizer = Adam(learning_rate=learning_rate)#1e-5
+
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+
+    path_to_save_model = './tmp'
+    if not os.path.isdir(path_to_save_model):
+        os.makedirs(path_to_save_model)
+
+    ckpt_saver = ModelCheckpoint(
+        path_to_save_model,
+        monitor="val_accuracy",
+        mode='max',
+        save_best_only=True,
+        save_freq='epoch',
+        verbose=1
+    )
+
+    model.fit(
+        train_generator,
+        steps_per_epoch=total_train_imgs // batch_size,
+        validation_data=val_generator,
+        validation_steps=total_val_imgs // batch_size,
+        epochs=epochs,
+        callbacks=[early_stopping, ckpt_saver]
+    )
+
+    print("[INFO] Evaluation phase...")
+
+    predictions = model.predict(eval_generator)
+    predictions_idxs = np.argmax(predictions, axis=1)
+
+    my_classification_report = classification_report(eval_generator.classes, predictions_idxs, 
+                                                     target_names=eval_generator.class_indices.keys())
+
+    my_confusion_matrix = confusion_matrix(eval_generator.classes, predictions_idxs)
+
+    print("[INFO] Classification report : ")
+    print(my_classification_report)
+
+    print("[INFO] Confusion matrix : ")
+    print(my_confusion_matrix)
+
+
+    print("Starting evaluation using model.evaluate")
+    scores = model.evaluate(eval_generator)
+    print("Done evaluating!")
+    loss = scores[0]
+    print(f"loss for hyptertune = {loss}")
+
+    now = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+    zipped_folder_name = f'trained_model_{now}_loss_{loss}'
+
+    shutil.make_archive(zipped_folder_name, 'zip', '/usr/src/app/tmp')
+
+    path_zipped_folder = '/usr/src/app/' + zipped_folder_name + '.zip'
+    upload_data_to_bucket(models_bucket_name, path_zipped_folder, zipped_folder_name)
+
+    hpt = hypertune.HyperTune()
+    hpt.report_hyperparameter_tuning_metric(hyperparameter_metric_tag='loss', 
+                                            metric_value=loss, global_step=epochs)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--bucket_name", type=str, help="Bucket name on google cloud storage",
+                        default="images-data-bucket")
+
+    parser.add_argument("--models_bucket_name", type=str, help="Bucket name on google cloud storage for saving trained models",
+                        default="trained_models_images_classification")
+
+    parser.add_argument("--batch_size", type=int, help="Batch size used by the deep learning model", 
+                        default=2)
+
+    parser.add_argument("--learning_rate", type=float, help="Batch size used by the deep learning model", 
+                        default=1e-5)
+
+    args = parser.parse_args()
+
+    print("Downloading of data started ...")
+    # download_data_to_local_directory(args.bucket_name, "./data")
+    print("Download finished!")
+
+    path_to_data = './data'
+    train(path_to_data, args.batch_size, 2, args.learning_rate, args.models_bucket_name)
